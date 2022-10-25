3656faeb2956a71bd4a96c0049e00d02806b9bc0
Anneparvin <108586988+Anneparvin@users.noreply.github.com>
2022-10-26 05:48:56 +0600

    add2

--- src/Shared/LeftSideNav/LeftSideNav.js
+++ src/Shared/LeftSideNav/LeftSideNav.js
@@ -1,10 +1,18 @@
 import React, { useState } from 'react';
+import { useEffect } from 'react';
 
 const LeftSideNav = () => {
     const [categories, setCategories] = useState([]);
+
+    useEffect(() =>{
+        fetch('http://localhost:5000/courses-categories')
+        .then (res => res.json())
+        .then(data => setCategories(data))
+    },[])
+
     return (
         <div>
-            <h2>All Catrgory</h2>
+            <h2>All Catrgory:{categories.length}</h2>
         </div>
     );
 };
