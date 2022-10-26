8e48d33d91efe6ed0b52c662b58b814f7fe71254
Anneparvin <108586988+Anneparvin@users.noreply.github.com>
2022-10-26 17:15:55 +0600

    addd

---
+++ src/Pages/Blog/Blog.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+const Blog = () => {
+    return (
+        <div>
+            {/* q-1 */}
+            <Card style={{ width: '45rem' }}>
+      <Card.Body>
+        <Card.Title>1..What is <strong>Cors?</strong></Card.Title>
+        <Card.Text>
+            <strong>
+            Cross-origin resource sharing (cors) is a mechanism that allows restricted resources on a web page to be requested from another domain outside the domain from which the first resource was served
+            </strong>
+            <strong>
+            A web page may freely embed cross-origin images, stylesheets, scripts, iframes, and videos. 
+            </strong>
+
+          
+        </Card.Text>
+        
+      </Card.Body>
+    </Card>
+            {/* q-2 */}
+            <Card style={{ width: '45rem' }}>
+      <Card.Body>
+        <Card.Title>2..For what purpose <strong>Firebase</strong> is used?What is the Altenative of <strong>Firebase</strong></Card.Title>
+        <Card.Text>
+            <strong>
+            Firebase helps you develop high-quality apps, grow your user base, and earn more money. Each feature works independently, and they work even better together.
+            </strong>
+            <strong>
+            whether you need an alternative to the entire Firebase platform or an alternative to a specific Firebase feature, here's a breakdown of other tools worth considering.
+            1..Parse
+            2..Supabase
+            3..Kuzzel
+            4..Back4App
+            </strong>
+
+          
+        </Card.Text>
+        
+      </Card.Body>
+    </Card>
+            {/* q-1 */}
+            <Card style={{ width: '45rem' }}>
+      <Card.Body>
+        <Card.Title>3..What is <strong>Node</strong>?How it is Work</Card.Title>
+        <Card.Text>
+            <strong>
+            It is a used as backend service where javascript works on the server-side of the application. This way javascript is used on both frontend and backend. Node. js runs on chrome v8 engine which converts javascript code into machine code, it is highly scalable, lightweight, fast, and data-intensive
+            </strong>
+            <strong>
+            the server consists of one thread processing one event after another. A new request coming in is one kind of event. The server starts processing it and when there is a blocking IO operation, it does not wait until it completes and instead registers a callback function.s
+            </strong>
+
+          
+        </Card.Text>
+        
+      </Card.Body>
+    </Card>
+            {/* q-4 */}
+            <Card style={{ width: '45rem' }}>
+      <Card.Body>
+        <Card.Title>4..How does <strong>private route</strong> work?</Card.Title>
+        <Card.Text>
+            <strong>
+            The private route component is similar to the public route, the only change is that redirect URL and authenticate condition. If the user is not authenticated he will be redirected to the login page and the user can only access the authenticated routes If he is authenticated (Logged in).
+            </strong>
+            <strong>
+            The private route component is used to protect selected pages in a React app from unauthenticated users.
+            </strong>
+
+          
+        </Card.Text>
+        
+      </Card.Body>
+    </Card>
+        </div>
+    );
+};
+
+export default Blog;
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,8 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
+import Blog from "../Pages/Blog/Blog";
 import Category from "../Pages/Category/Category";
 import Courses from "../Pages/Courses/Courses";
 import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
+import Register from "../Pages/Register/Register";
 
 export const routes = createBrowserRouter([
 
@@ -24,6 +27,18 @@ export const routes = createBrowserRouter([
              path:'/courses/:id',
              element:<Courses></Courses>,
               loader: ({params}) =>fetch (`http://localhost:5000/courses/${params.id}`)
+            },
+            {
+                path:'/login',
+                element:<Login></Login> 
+            },
+            {
+                path:'/register',
+                element:<Register></Register>
+            },
+            {
+                path:'/Blog',
+                element:<Blog></Blog>
             }
          ]
     }
